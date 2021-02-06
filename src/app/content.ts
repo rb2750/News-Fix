7626e2b8e3378f46f600c31aa21124eff72ebb15
Rob <rob@bladen.me>
2021-02-06 23:50:09 +0000

    Fixed a bug that's appeared in the site

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,12 +1,6 @@
 chrome.runtime.sendMessage({}, (response) => {
-    var checkReady = setInterval(() => {
-        if (document.readyState === "complete") {
-            clearInterval(checkReady)
-
-            var script = document.createElement('script');
-            script.textContent = "window.EV = undefined;";
-            (document.head || document.documentElement).appendChild(script);
-            script.remove();
-        }
-    })
+    var script = document.createElement('script');
+    script.textContent = "window.EV = undefined;";
+    (document.head || document.documentElement).appendChild(script);
+    script.remove();
 })
