9f453d143761f1e62f9fa0933c8d3a62a4d3efe3
Rob <rob@bladen.me>
2021-03-01 18:45:56 +0000

    Run on all news sites. Disable timeout instead.

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,6 +1,14 @@
 chrome.runtime.sendMessage({}, (response) => {
+    var article = false;
+
+    for (var i of document.getElementsByTagName("script")) {
+      if (i.type == "application/ld+json" ? JSON.parse(i.innerHTML)["@type"] == "NewsArticle" : "") {
+        article = true;
+        break;
+      }
+    }
     var script = document.createElement('script');
-    script.textContent = "window.EV = undefined;";
+    script.textContent = "window.setTimeout = function(){};";
     (document.head || document.documentElement).appendChild(script);
     script.remove();
 })
